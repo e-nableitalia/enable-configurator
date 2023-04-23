0f71a46e4f322767f4efd1f2383a73c161cd15c6
kabirlovero <kabirlovero@gmail.com>
2023-04-23 20:05:30 +0200

    Fixing gitignore

--- Configurator/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Configurator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
