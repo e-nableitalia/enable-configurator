cbe53a43f8e69a3437f404576aeb6ff8543269e4
Alberto Navatta <anavatta@imnet.com>
2023-05-20 23:04:38 +0200

    Adjusted layout and added F dimension

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -8,22 +8,22 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 // import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 
-import {STLLoader} from "three/examples/jsm/loaders/STLLoader";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { easing } from 'maath'
 import { useSnapshot } from 'valtio'
 import { state } from './store'
 
 export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
-  
-   <Canvas shadows camera={{ position, fov }} gl={{ preserveDrawingBuffer: true }} eventSource={document.getElementById('root')} eventPrefix="client">
+
+  <Canvas shadows camera={{ position, fov }} gl={{ preserveDrawingBuffer: true }} eventSource={document.getElementById('root')} eventPrefix="client">
     <ambientLight intensity={0.5} />
-     <Environment preset="city" />
-     <CameraRig>
-       <Backdrop /> 
-         <BikeAdapter />
-       </CameraRig>
-     <OrbitControls />
-   </Canvas>
+    <Environment preset="city" />
+    <CameraRig>
+      <Backdrop />
+      <BikeAdapter />
+    </CameraRig>
+    <OrbitControls />
+  </Canvas>
 )
 
 function Backdrop() {
@@ -57,28 +57,23 @@ function BikeAdapter(props) {
     metalness: .9, roughness: 0.6, color: 0x0ad2ff
   })
 
-  const geom = useLoader(STLLoader, "/defaultBikeAdapter.stl") 
+  const geom = useLoader(STLLoader, "/defaultBikeAdapter.stl")
 
   const ref = useRef();
-  const {camera} = useThree();
+  const { camera } = useThree();
   useEffect(() => {
-      camera.lookAt(ref.current.position);
+    camera.lookAt(ref.current.position);
   });
 
   console.log(geom)
 
   useFrame((state, delta) => easing.dampC(material.color, snap.color, 0.25, delta))
   return (
-    <mesh ref={ref} castShadow rotation-x = { 0.5 } rotation-z = { 0.9 } rotation-y = { -1 } geometry={geom} position = {[0.2, -0.1, 0.26]} scale={[.006, .006, .006]} material={material} material-roughness={1} {...props} dispose={null}>
-      {/* <Decal position={[0, 0.04, 0.15]} rotation={[0, 0, 0]} scale={0.15} map={texture} map-anisotropy={16} /> */}
+    <mesh ref={ref} castShadow rotation-x={0.5} rotation-z={0.9} rotation-y={-1} geometry={geom} position={[0.2, -0.1, 0.26]} scale={[.006, .006, .006]} material={material} material-roughness={1} {...props} dispose={null}>
     </mesh>
   )
-
-      //<mesh castShadow geometry={nodes.originalBikeAdapter.geometry} material={materials.lambert1} material-roughness={1} {...props} dispose={null}>
-    //  <Decal position={[0, 0.04, 0.15]} rotation={[0, 0, 0]} scale={0.15} map={texture} map-anisotropy={16} />
-    //</mesh>
 }
 
 //useGLTF.preload('/originalBikeAdapter.glb')
-useGLTF.preload('/shirt_baked_collapsed.glb')
-;['/react.png', '/three2.png', '/pmndrs.png'].forEach(useTexture.preload)
+//useGLTF.preload('/shirt_baked_collapsed.glb')
+//;['/react.png', '/three2.png', '/pmndrs.png'].forEach(useTexture.preload)
--- frontend/src/Overlay.js
+++ frontend/src/Overlay.js
@@ -7,8 +7,9 @@ import React, { useState } from "react";
 import { Slider } from "primereact/slider";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
+import { Dropdown } from 'primereact/dropdown';
 import { motion, AnimatePresence } from 'framer-motion'
-import { AiFillCamera, AiOutlineArrowLeft, AiOutlineHighlight, AiOutlineShopping } from 'react-icons/ai'
+import { AiFillTool, AiOutlineArrowLeft, AiOutlineHighlight } from 'react-icons/ai'
 import { useSnapshot } from 'valtio'
 import { state } from './store'
 
@@ -37,9 +38,9 @@ export function Overlay() {
 								initial={{ x: 100, opacity: 0 }}
 								animate={{ x: 0, opacity: 1 }}
 								transition={{ type: 'spring', damping: 5, stiffness: 40, restDelta: 0.001, duration: 0.3 }}>
-								<h2>e-Nable Italia BIKE ADAPTER.</h2>
+								<h2>e-Nable Italia STL Generator</h2>
 							</motion.div>
-							<div className="support--content" >
+							{/* <div className="support--content" > */}
 								<motion.div
 									key="p"
 									initial={{ y: 100, opacity: 0 }}
@@ -54,13 +55,13 @@ export function Overlay() {
 										delayChildren: 0.2
 									}}>
 									<p>
-										Create your unique Bike Adapter with our brand-new 3D customization tool. <strong>Customize</strong> on your needs to fit with recipient characteristics.
+										Create your unique Device with our brand-new 3D customization tool. <strong>Customize</strong> on your needs to fit with recipient characteristics.
 									</p>
 									<button style={{ background: snap.color }} onClick={() => (state.intro = false)}>
 										CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
 									</button>
 								</motion.div>
-							</div>
+							{/* </div> */}
 						</div>
 					</motion.section>
 				) : (
@@ -74,65 +75,85 @@ export function Overlay() {
 }
 
 function Customizer() {
-	const snap = useSnapshot(state)
-  const [arm, setArm] = useState(50);
-  const [affectedarm, setAffectedArm] = useState(50);
+	const snap = useSnapshot(state);
+	const [device, setDevice] = useState(null);
+	const [arm, setArm] = useState(50);
+	const [affectedarm, setAffectedArm] = useState(50);
 	const [cone, setCone] = useState(50);
-	const [coneb, setConeb] = useState(50);  
-  const [handle, setHandle] = useState(50);
+	const [coneb, setConeb] = useState(50);
+	const [handle, setHandle] = useState(50);
+	const [affectedarminner, setAffectedArmInner] = useState(50);
+	const deviceItems = [
+		{label: 'Bike Adapter', value: 'BikeAdapter'}
+	];
 	return (
 
 		<div className="customizer">
-        <div className="field grid">
-          <label for="color" class="col-fixed" style={{ "width" : "200px" }}>Device Color</label>
-          <div class="col">
-            <div id="color" className="color-options">
-              {snap.colors.map((color) => (
-                <div key={color} className={`circle`} style={{ background: color }} onClick={() => (state.color = color)}></div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="field grid">
-          <label for="arm" class="col-fixed" style={{ "width" : "200px" }}>A - Arm Length</label>
-          <div class="col">
-              <InputText value={arm} onChange={(e) => setArm(e.target.value)} className="w-full" />
-              <Slider id="arm" value={arm} onChange={(e) => setArm(e.value)} className="w-full" />
-          </div>
-        </div>
-        <div className="field grid">
-          <label for="affected-arm" class="col-fixed" style={{ "width" : "200px" }}>B - Affected Arm Length</label>
-          <div class="col">
-              <InputText value={affectedarm} onChange={(e) => setArm(e.target.value)} className="w-full" />
-              <Slider id="affected-arm" value={affectedarm} onChange={(e) => setAffectedArm(e.value)} className="w-full" />
-          </div>
-        </div>
+			<card>
+				<div classname="field grid">
+					<label for="color" class="col-fixed" style={{ "width": "300px" }}>Select Device</label>
+					<div class="col">
+						<Dropdown value={device} options={deviceItems} onChange={(e) => setDevice(e.value)} placeholder="Select device model"/>
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="color" class="col-fixed" style={{ "width": "300px" }}>Device Color</label>
+					<div class="col">
+						<div id="color" className="color-options">
+							{snap.colors.map((color) => (
+								<div key={color} className={`circle`} style={{ background: color }} onClick={() => (state.color = color)}></div>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="arm" class="col-fixed" style={{ "width": "300px" }}>A - Arm Length</label>
+					<div class="col">
+						<InputText value={arm} onChange={(e) => setArm(e.target.value)} className="w-full" />
+						<Slider id="arm" value={arm} onChange={(e) => setArm(e.value)} className="w-full" />
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="affected-arm" class="col-fixed" style={{ "width": "300px" }}>B - Affected Arm Length</label>
+					<div class="col">
+						<InputText value={affectedarm} onChange={(e) => setArm(e.target.value)} className="w-full" />
+						<Slider id="affected-arm" value={affectedarm} onChange={(e) => setAffectedArm(e.value)} className="w-full" />
+					</div>
+				</div>
 
-        <div className="field grid">
-          <label for="cone" class="col-fixed" style={{ "width" : "200px" }}>C - Cone Length Top</label>
-          <div class="col">
-              <InputText value={cone} onChange={(e) => setCone(e.target.value)} className="w-full" />
-              <Slider id="cone" value={cone} onChange={(e) => setCone(e.value)} className="w-full" />
-          </div>
-        </div>
-        <div className="field grid">
-          <label for="coneb" class="col-fixed" style={{ "width" : "200px" }}>D - Cone Length Bottom</label>
-          <div class="col">
-              <InputText value={coneb} onChange={(e) => setCone(e.target.value)} className="w-full" />
-              <Slider id="coneb" value={coneb} onChange={(e) => setConeb(e.value)} className="w-full" />
-          </div>
-        </div>        
-        <div className="field grid">
-          <label for="radius" class="col-fixed" style={{ "width" : "200px" }}>E - Handle Radius</label>
-          <div class="col">
-            <InputText value={handle} onChange={(e) => setHandle(e.target.value)} className="w-full" />
-            <Slider id="radius" value={handle} onChange={(e) => setHandle(e.value)} className="w-full" />
-          </div>
-        </div>
+				<div className="field grid">
+					<label for="cone" class="col-fixed" style={{ "width": "300px" }}>C - Cone Length Top</label>
+					<div class="col">
+						<InputText value={cone} onChange={(e) => setCone(e.target.value)} className="w-full" />
+						<Slider id="cone" value={cone} onChange={(e) => setCone(e.value)} className="w-full" />
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="coneb" class="col-fixed" style={{ "width": "300px" }}>D - Cone Length Bottom</label>
+					<div class="col">
+						<InputText value={coneb} onChange={(e) => setCone(e.target.value)} className="w-full" />
+						<Slider id="coneb" value={coneb} onChange={(e) => setConeb(e.value)} className="w-full" />
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="radius" class="col-fixed" style={{ "width": "300px" }}>E - Handle Radius</label>
+					<div class="col">
+						<InputText value={handle} onChange={(e) => setHandle(e.target.value)} className="w-full" />
+						<Slider id="radius" value={handle} onChange={(e) => setHandle(e.value)} className="w-full" />
+					</div>
+				</div>
+				<div className="field grid">
+					<label for="affectedarminner" class="col-fixed" style={{ "width": "300px" }}>F - Affected Arm Inner Length</label>
+					<div class="col">
+						<InputText value={affectedarminner} onChange={(e) => setAffectedArmInner(e.target.value)} className="w-full" />
+						<Slider id="affectedarminner" value={affectedarminner} onChange={(e) => setAffectedArmInner(e.value)} className="w-full" />
+					</div>
+				</div>
+			</card>
 
-      <Card role="region">
-          add space here for properly positioning
-      </Card>
+			<Card role="region">
+				Change values to generate a custom Device
+			</Card>
 
 			<button
 				className="share"
@@ -143,13 +164,13 @@ function Customizer() {
 					link.setAttribute('href', document.querySelector('canvas').toDataURL('image/png').replace('image/png', 'image/octet-stream'))
 					link.click()
 				}}>
-				DOWNLOAD
-				<AiFillCamera size="1.3em" />
+				GENERATE
+				<AiFillTool size="1.3em" />
 			</button>
 			<button className="exit" style={{ background: snap.color }} onClick={() => (state.intro = true)}>
 				GO BACK
 				<AiOutlineArrowLeft size="1.3em" />
 			</button>
 		</div>
-	) 
+	)
 }
