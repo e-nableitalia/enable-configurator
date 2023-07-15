9c76d1bf7b9bbb979bbf5fdfa50166efb3461048
Alberto Navatta <alberto@e-nableitalia.it>
2023-07-15 11:16:27 +0000

    updated package-lock

---
+++ frontend/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "building project"
+npm update
+npm run build
+echo "making project backup backup"
+tar -czf /home/centos/configurator_ui_backup_$(date +'%Y%m%d').tgz /var/www/html/configurator
+echo "deploying new solution"
+rm -rf /var/www/html/configurator
+mkdir /var/www/html/configurator/
+cp -r /home/centos/enable-configurator/frontend/build/* /var/www/html/configurator/
